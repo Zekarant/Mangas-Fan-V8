224a826b89eca5d41ca1fbf76c23c550c3dfccce
Amphi62 <nicolas.deville62160@outlook.fr>
2023-01-07 15:27:13 +0100

    refactor(template): use snake case for templates names and templates attributes give by controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,33 +6,22 @@ use App\Repository\AnimesRepository;
 use App\Repository\ArticlesAnimeRepository;
 use App\Repository\NewsRepository;
 use App\Repository\TomeMangasRepository;
-use App\Service\WebhookDiscordService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class HomeController extends AbstractController
 {
-    private WebhookDiscordService $webhookDiscordService;
-
-    public function __construct(WebhookDiscordService $webhookDiscordService) {
-        $this->webhookDiscordService = $webhookDiscordService;
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     */
     #[Route('/', name: 'app_home')]
     public function index(NewsRepository $newsRepository, AnimesRepository $animesRepository, TomeMangasRepository $tomeMangasRepository, ArticlesAnimeRepository $articlesAnimeRepository): Response
     {
 
         return $this->render('home/index.html.twig', [
             'news' => $newsRepository->findBy(array(), array('id' => 'DESC'), 3),
-            'lastAnimes' => $animesRepository->findBy(array(), array('id' => 'DESC'), 6),
-            'coupdeCoeur' => $animesRepository->findOneBy(array('coupCoeur' => 1)),
-            'lastMangas' => $tomeMangasRepository->findBy(array(), array('createdAt' => 'DESC'), 6),
-            'lastArticlesAnime' => $articlesAnimeRepository->findBy(array(), array('createdAt' => 'DESC'), 4)
+            'last_animes' => $animesRepository->findBy(array(), array('id' => 'DESC'), 6),
+            'month_favorite' => $animesRepository->findOneBy(array('coupCoeur' => 1)),
+            'last_mangas' => $tomeMangasRepository->findBy(array(), array('createdAt' => 'DESC'), 6),
+            'last_articles_anime' => $articlesAnimeRepository->findBy(array(), array('createdAt' => 'DESC'), 4)
         ]);
     }
 }
--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Comments;
 use App\Entity\News;
 use App\Form\Type\CommentType;
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,9 +21,9 @@ class NewsController extends AbstractController
         $comment = new Comments($news);
         $commentForm = $this->createForm(CommentType::class, $comment);
 
-        return $this->renderForm('news/show.html.twig', [
+        return $this->renderForm('news/news_page.html.twig', [
             'news' => $news,
-            'commentForm' => $commentForm,
+            'comment_form' => $commentForm,
         ]);
     }
 }
