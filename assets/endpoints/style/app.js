5496190765c0183197db579a2c99d967816569cb
Nicolas Deville <ndeville@norsys.fr>
2024-05-07 15:52:07 +0200

    feat(T-019): update entries webpack config + update database values

--- assets/endpoints/style/app.js
+++ assets/endpoints/style/app.js
@@ -2,8 +2,8 @@
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
-import '../css/app.scss';
-import '../css/composants/navbar.scss';
-import '../css/composants/header.scss';
+import '../../css/app.scss';
+import '../../css/composants/navbar.scss';
+import '../../css/composants/header.scss';
 
 // import '../css/news.scss';
---
+++ assets/endpoints/style/news.js
@@ -0,0 +1,1 @@
+import '../../css/news.scss';
